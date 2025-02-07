1644a4302f6ca78f5c36204205c91c4da12e3e3d
deswalcodes <21cs2014@rgipt.ac.in>
2025-02-07 17:45:36 +0530

    learning ssr

---
+++ app/user/page.tsx
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export default async function User(){
+
+   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+   
+   const data = response.data;
+
+
+
+    return <div>
+        User Page
+        <br/>
+        {data.name} <br/>
+        {data.email}
+    </div>
+}
